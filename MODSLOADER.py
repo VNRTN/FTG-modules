58dd9614176db7c9c682f43d2f5594c257423510
VNRTN <vnaratan@gmail.com>
2021-02-28 20:05:53 +0300

    Update MODSLOADER.py

--- MODSLOADER.py
+++ MODSLOADER.py
@@ -7,5 +7,5 @@ class loadermodsgit(loader.Module):
 	async def mldcmd(self, message):
 		text = utils.get_args_raw(message)
 		await message.edit(f'LOL')
-        await sleep(5)
+		await sleep(5)
 		await message.edit(f'LOL1')
