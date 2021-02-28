00c042ef53b1995a49d7f8e71d09b428ff9317d9
VNRTN <vnaratan@gmail.com>
2021-02-28 19:26:21 +0300

    new modules

--- MODSLOADER.py
+++ MODSLOADER.py
@@ -7,6 +7,5 @@ class LinkInTextMod(loader.Module):
 	async def MLDcmd(self, message):
 		text = utils.get_args_raw(message)
 		await message.edit(f'LOL')
+        await sleep 2
         await message.edit(f'LOL1')
-        await message.edit(f'LOL2')
-        await message.edit(f'LOL3!')
