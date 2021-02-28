4fc363ee13ac7dd212cc64ac5003b1e9000dd965
VNRTN <vnaratan@gmail.com>
2021-02-28 20:02:46 +0300

    Update MODSLOADER.py

--- MODSLOADER.py
+++ MODSLOADER.py
@@ -1,9 +1,11 @@
 from .. import loader, utils
 
 @loader.tds
-class LinkInTextMod(loader.Module):
-	strings = {"name": "Хуй знает"}
+class loadermodsgit(loader.Module):
+	strings = {"name": "loadermodsgit"}
 	@loader.owner
 	async def mldcmd(self, message):
 		text = utils.get_args_raw(message)
 		await message.edit(f'LOL')
+        await sleep(5)
+		await message.edit(f'LOL1')
