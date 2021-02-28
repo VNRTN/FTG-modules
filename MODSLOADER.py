2efdf056bbb738eabcee325a929509cfcc1abb73
VNRTN <vnaratan@gmail.com>
2021-02-28 19:43:15 +0300

    1

--- MODSLOADER.py
+++ MODSLOADER.py
@@ -5,7 +5,7 @@ class MODSLOADER(loader.Module):
 	strings = {"name": "MODSLOADER"}
 	@loader.owner
 	async def MLDcmd(self, message):
-        Delay = float('5')
+		Delay = float('4')
 		text = utils.get_args_raw(message)
 		await message.edit(f'LOL')
 		await sleep (Delay)
