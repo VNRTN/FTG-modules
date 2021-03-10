a2f0aaaa07f17caf202aaccb3d9c38586c2fc339
VNRTN <vnaratan@gmail.com>
2021-03-10 23:03:35 +0300

    add module

---
+++ tts.py
@@ -0,0 +1,69 @@
+# requires: gtts hachoir
+
+from gtts import gTTS
+import io
+import requests
+from .. import loader, utils
+
+def register(cb):
+    cb(TTSMod())
+
+
+class TTSMod(loader.Module):
+    """Озвучивание текста"""
+
+    strings = {"name": "Text to speech",
+               "tts_lang_cfg": "Set your language code for the TTS here.",
+               "tts_needs_text": "<code>I need some text to convert to speech!</code>",
+               "no_text": "Мне нечего говорить"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig("TTS_LANG", "en", lambda m: self.strings("tts_lang_cfg", m))
+
+    async def say(self, message, speaker, text, file=".dtts.mp3"):
+        if not text:
+            return await utils.answer(message, self.strings["no_text"])
+
+        reply = await message.get_reply_message()
+        await message.delete()
+        data = {"text": text}
+        if speaker:
+            data.update({"speaker": speaker})
+
+        # creating file in memory
+        f = io.BytesIO(requests.get("https://station.aimylogic.com/generate", data=data).content)
+        f.name = file
+
+        await message.client.send_file(message.to_id, f, voice_note=True, reply_to=reply)
+
+    async def ttslcmd(self, message):
+        """Levitan voice"""
+        await self.say(message, "levitan", utils.get_args_raw(message))
+
+    async def ttsocmd(self, message):
+        """Oksana voice"""
+        await self.say(message, "oksana", utils.get_args_raw(message))
+
+    async def ttsycmd(self, message):
+        """Yandex voice"""
+        await self.say(message, None, utils.get_args_raw(message))
+
+    @loader.unrestricted
+    @loader.ratelimit
+    async def ttscmd(self, message):
+        """Google voice"""
+        text = utils.get_args_raw(message.message)
+        if len(text) == 0:
+            if message.is_reply:
+                text = (await message.get_reply_message()).message
+            else:
+                await utils.answer(message, self.strings("tts_needs_text", message))
+                return
+
+        tts = await utils.run_sync(gTTS, text, lang=self.config["TTS_LANG"])
+        voice = BytesIO()
+        await utils.run_sync(tts.write_to_fp, voice)
+        voice.seek(0)
+        voice.name = "voice.mp3"
+
+        await utils.answer(message, voice, voice_note=True)
